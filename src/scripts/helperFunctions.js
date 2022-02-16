71be16ce0d700f84c74c18fd0520ec40338c5a99
TheJuze <dimanikus0106@gmail.com>
2022-02-16 15:42:58 +0300

    fix(header): header navigation and scroll disabling
    Refs: CL-1

--- src/scripts/helperFunctions.js
+++
@@ -1,9 +0,0 @@
-
-function changeBodyOverflow() { // TODO: make working
-    console.log('function called');
-    if (document.getElementById('menu__toggle').checked) {
-        document.body.style.overflow = 'hidden';
-    } else {
-        document.body.style.overflow = 'unset';
-    }
-}
